a1f9547ab4eb1a83d4c6b0eb10dd85999218dde4
dragoryu <sanchith.krishnan@gmail.com>
2025-11-08 03:45:17 +0000

    Updating Pyright with custom settings

--- init.lua
+++ init.lua
@@ -681,7 +681,18 @@ require('lazy').setup({
       local servers = {
         -- clangd = {},
         gopls = {},
-        pyright = {},
+        pyright = {
+          settings = {
+            python = {
+              analysis = {
+                autoSearchPaths = true,
+                diagnosticMode = 'workspace',
+                useLibraryCodeForTypes = true,
+                extraPaths = { '.', 'src', 'config', 'utils', 'app' },
+              },
+            },
+          },
+        },
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
