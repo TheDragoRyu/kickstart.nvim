34cb865577b4d7b9b2c18d2cc800b197092626f0
sanchith_krishnan <sanchith.krishnan@gmail.com>
2026-04-24 09:07:18 +0000

    feat(cs): add /// XML doc expansion, tag auto-close, and /// Enter continuation

--- lua/plugins/roslyn.lua
+++ lua/plugins/roslyn.lua
@@ -64,6 +64,55 @@ return {
       })
 
       require('roslyn').setup(opts)
+
+      -- /// → XML doc summary block
+      vim.api.nvim_create_autocmd('TextChangedI', {
+        pattern = { '*.cs', '*.cshtml', '*.razor' },
+        callback = function()
+          local line = vim.api.nvim_get_current_line()
+          local indent = line:match '^(%s*)///$'
+          if not indent then return end
+          local row = vim.api.nvim_win_get_cursor(0)[1]
+          local t = { indent .. '/// <summary>', indent .. '/// ', indent .. '/// </summary>' }
+          vim.api.nvim_buf_set_lines(0, row - 1, row, false, t)
+          vim.api.nvim_win_set_cursor(0, { row + 1, #indent + 5 })
+        end,
+      })
+
+      -- <CR> on a /// line continues with /// on next line
+      vim.api.nvim_create_autocmd('BufEnter', {
+        pattern = { '*.cs', '*.cshtml', '*.razor' },
+        callback = function()
+          vim.keymap.set('i', '<CR>', function()
+            local line = vim.api.nvim_get_current_line()
+            local indent = line:match '^(%s*)///'
+            if indent then
+              return '<CR>' .. indent .. '/// '
+            end
+            return '<CR>'
+          end, { buffer = true, expr = true })
+        end,
+      })
+
+      -- Auto-close XML doc tags on >  (e.g. <param name="x"> → <param name="x"></param>)
+      vim.api.nvim_create_autocmd('TextChangedI', {
+        pattern = { '*.cs', '*.cshtml', '*.razor' },
+        callback = function()
+          local line = vim.api.nvim_get_current_line()
+          -- only inside doc comment lines
+          if not line:match '^%s*///' then return end
+          local col = vim.api.nvim_win_get_cursor(0)[2]
+          local before = line:sub(1, col)
+          -- match last unclosed opening tag (not self-closing, not closing tag)
+          local tag = before:match '<([%w]+)[^/]*>$'
+          if not tag then return end
+          -- avoid double-inserting
+          local after = line:sub(col + 1)
+          if after:match('^</' .. tag .. '>') then return end
+          local new_line = before .. '</' .. tag .. '>' .. after
+          vim.api.nvim_set_current_line(new_line)
+        end,
+      })
     end,
   },
 }
