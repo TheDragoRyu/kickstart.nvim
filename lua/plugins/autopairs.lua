55f6ed4a67f0d8478575a5f6513f14035f590b8d
sanchith_krishnan <sanchith.krishnan@gmail.com>
2026-04-24 15:01:05 +0000

    feat: add rider-style csharp autopairs

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -4,5 +4,32 @@
 return {
   'windwp/nvim-autopairs',
   event = 'InsertEnter',
-  opts = {},
+  config = function()
+    local autopairs = require 'nvim-autopairs'
+    local Rule = require 'nvim-autopairs.rule'
+    local cond = require 'nvim-autopairs.conds'
+    local csharp_filetypes = { 'cs', 'cshtml', 'razor' }
+
+    autopairs.setup {
+      -- Built-ins cover Rider-style (), [], {}, quotes, backticks, pair
+      -- deletion, moveright, and <CR> splitting.
+    }
+
+    autopairs.add_rules {
+      -- Close <> for C# generic type arguments, but avoid common comparisons.
+      Rule('<', '>', csharp_filetypes)
+        :with_pair(cond.before_regex('[%w_%)]', 1))
+        :with_pair(cond.not_after_regex('[%w_%.]', 1))
+        :with_move(function(opts)
+          return opts.char == '>'
+        end),
+
+      -- Rider-like completion for C# raw string literals: """|""".
+      Rule('"""', '"""', 'cs'):with_pair(cond.not_before_char('"', 3)),
+
+      -- Rider closes block comments in C# and markup comments in Razor views.
+      Rule('/*', '*/', csharp_filetypes):with_pair(cond.not_after_regex('%*', 1)),
+      Rule('<!--', '-->', { 'cshtml', 'razor' }):with_cr(cond.none()),
+    }
+  end,
 }
