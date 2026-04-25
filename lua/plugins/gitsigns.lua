3fe5690a5724bd2da45ef888f27a8a98a2d2d484
sanchith_krishnan <sanchith.krishnan@gmail.com>
2026-04-25 13:45:53 +0000

    feat: show inline git blame hints

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -8,6 +8,15 @@ return {
       topdelete = { text = '‾' },
       changedelete = { text = '~' },
     },
+    current_line_blame = true,
+    current_line_blame_opts = {
+      virt_text = true,
+      virt_text_pos = 'right_align',
+      delay = 400,
+      ignore_whitespace = false,
+      virt_text_priority = 100,
+    },
+    current_line_blame_formatter = '<author>, <author_time:%R> - <summary>',
     on_attach = function(bufnr)
       local gitsigns = require 'gitsigns'
 
