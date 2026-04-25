689b600c270ac46925fb7eabd4d5c457d067154a
sanchith_krishnan <sanchith.krishnan@gmail.com>
2026-04-25 11:43:38 +0000

    feat: make gd show callers on definitions

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -132,6 +132,109 @@ return {
       end
 
       local methods = vim.lsp.protocol.Methods
+      local definition_method = methods.textDocument_definition or 'textDocument/definition'
+      local prepare_call_hierarchy_method = methods.textDocument_prepareCallHierarchy or 'textDocument/prepareCallHierarchy'
+      local incoming_calls_method = methods.callHierarchy_incomingCalls or 'callHierarchy/incomingCalls'
+
+      ---@param client vim.lsp.Client
+      ---@param method vim.lsp.protocol.Method|string
+      ---@param bufnr? integer some lsp support methods only in specific files
+      ---@return boolean
+      local function client_supports_method(client, method, bufnr)
+        if vim.fn.has 'nvim-0.11' == 1 then
+          return client:supports_method(method, bufnr)
+        else
+          return client.supports_method(method, { bufnr = bufnr })
+        end
+      end
+
+      local function as_lsp_location_list(result)
+        if not result then
+          return {}
+        end
+
+        if result.uri or result.targetUri then
+          return { result }
+        end
+
+        return result
+      end
+
+      local function position_is_in_range(position, range)
+        if not position or not range then
+          return false
+        end
+
+        if position.line < range.start.line or position.line > range['end'].line then
+          return false
+        end
+
+        if position.line == range.start.line and position.character < range.start.character then
+          return false
+        end
+
+        if position.line == range['end'].line and position.character > range['end'].character then
+          return false
+        end
+
+        return true
+      end
+
+      local function definition_result_contains_position(result, bufnr, position)
+        local current_uri = vim.uri_from_bufnr(bufnr)
+
+        for _, location in ipairs(as_lsp_location_list(result)) do
+          local uri = location.targetUri or location.uri
+          local range = location.targetSelectionRange or location.range or location.targetRange
+
+          if uri == current_uri and position_is_in_range(position, range) then
+            return true
+          end
+        end
+
+        return false
+      end
+
+      local function as_call_hierarchy_item_list(result)
+        if not result then
+          return {}
+        end
+
+        if result.uri then
+          return { result }
+        end
+
+        return result
+      end
+
+      local function format_call_hierarchy_item(item)
+        if item.detail and item.detail ~= '' then
+          return string.format('%s - %s', item.name, item.detail)
+        end
+
+        return item.name
+      end
+
+      local function incoming_call_locations(result)
+        local locations = {}
+
+        for _, call in ipairs(result or {}) do
+          local from = call.from
+
+          if from and from.uri then
+            for _, range in ipairs(call.fromRanges or {}) do
+              locations[#locations + 1] = {
+                uri = from.uri,
+                range = range,
+                text = format_call_hierarchy_item(from),
+              }
+            end
+          end
+        end
+
+        return locations
+      end
+
       vim.lsp.handlers[methods.textDocument_publishDiagnostics] = function(err, result, ctx, config)
         if result and result.diagnostics and is_csharp_lsp_client(ctx.client_id) then
           local bufnr = vim.uri_to_bufnr(result.uri)
@@ -165,10 +268,144 @@ return {
           local telescope = require 'telescope.builtin'
           local uses_direct_navigation = client and (client.name == 'roslyn' or client.name == 'easy_dotnet')
           local uses_csharp_lens = uses_direct_navigation
+          local goto_definition = function()
+            if uses_direct_navigation then
+              vim.lsp.buf.definition()
+            else
+              telescope.lsp_definitions()
+            end
+          end
+
+          local goto_references_as_callers = function()
+            telescope.lsp_references { include_declaration = false }
+          end
 
           local map = function(keys, func, desc, mode)
             mode = mode or 'n'
             vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
+          end
+
+          local function open_incoming_calls_picker(locations)
+            local opts = { bufnr = event.buf }
+            local conf = require('telescope.config').values
+            local finders = require 'telescope.finders'
+            local make_entry = require 'telescope.make_entry'
+            local pickers = require 'telescope.pickers'
+            local quickfix_entries = vim.tbl_map(function(location)
+              return {
+                filename = vim.uri_to_fname(location.uri),
+                lnum = location.range.start.line + 1,
+                col = location.range.start.character + 1,
+                text = location.text,
+              }
+            end, locations)
+
+            pickers
+              .new(opts, {
+                prompt_title = 'LSP Incoming Calls',
+                finder = finders.new_table {
+                  results = quickfix_entries,
+                  entry_maker = make_entry.gen_from_quickfix(opts),
+                },
+                previewer = conf.qflist_previewer(opts),
+                sorter = conf.generic_sorter(opts),
+                push_cursor_on_edit = true,
+                push_tagstack_on_edit = true,
+              })
+              :find()
+          end
+
+          local function jump_to_incoming_call(location)
+            vim.lsp.util.show_document({
+              uri = location.uri,
+              range = location.range,
+            }, client.offset_encoding, { reuse_win = true, focus = true })
+          end
+
+          local function handle_incoming_calls(result)
+            local locations = incoming_call_locations(result)
+
+            if #locations == 0 then
+              vim.notify('No callers found', vim.log.levels.INFO)
+            elseif #locations == 1 then
+              jump_to_incoming_call(locations[1])
+            else
+              open_incoming_calls_picker(locations)
+            end
+          end
+
+          local function request_incoming_calls(item)
+            client:request(incoming_calls_method, { item = item }, function(err, result)
+              vim.schedule(function()
+                if err then
+                  vim.notify('Incoming calls unavailable: ' .. err.message, vim.log.levels.WARN)
+                  goto_references_as_callers()
+                  return
+                end
+
+                handle_incoming_calls(result)
+              end)
+            end, event.buf)
+          end
+
+          local function choose_call_hierarchy_item(items)
+            if #items == 0 then
+              goto_references_as_callers()
+            elseif #items == 1 then
+              request_incoming_calls(items[1])
+            else
+              vim.ui.select(items, {
+                prompt = 'Select call hierarchy item:',
+                format_item = format_call_hierarchy_item,
+              }, function(item)
+                if item then
+                  request_incoming_calls(item)
+                end
+              end)
+            end
+          end
+
+          local function goto_incoming_calls_or_references()
+            if
+              not client
+              or not client_supports_method(client, prepare_call_hierarchy_method, event.buf)
+              or not client_supports_method(client, incoming_calls_method, event.buf)
+            then
+              goto_references_as_callers()
+              return
+            end
+
+            local params = vim.lsp.util.make_position_params(0, client.offset_encoding)
+            client:request(prepare_call_hierarchy_method, params, function(err, result)
+              vim.schedule(function()
+                if err then
+                  vim.notify('Call hierarchy unavailable: ' .. err.message, vim.log.levels.WARN)
+                  goto_references_as_callers()
+                  return
+                end
+
+                choose_call_hierarchy_item(as_call_hierarchy_item_list(result))
+              end)
+            end, event.buf)
+          end
+
+          local function goto_definition_or_callers()
+            if not client or not client_supports_method(client, definition_method, event.buf) then
+              goto_definition()
+              return
+            end
+
+            local params = vim.lsp.util.make_position_params(0, client.offset_encoding)
+            client:request(definition_method, params, function(err, result)
+              vim.schedule(function()
+                if err or not definition_result_contains_position(result, event.buf, params.position) then
+                  goto_definition()
+                  return
+                end
+
+                goto_incoming_calls_or_references()
+              end)
+            end, event.buf)
           end
 
           map('gn', vim.lsp.buf.rename, '[R]e[n]ame')
@@ -198,23 +435,11 @@ return {
           end, '[G]oto Code [A]ction', { 'n', 'x' })
           map('gr', uses_direct_navigation and vim.lsp.buf.references or telescope.lsp_references, '[G]oto [R]eferences')
           map('gi', uses_direct_navigation and vim.lsp.buf.implementation or telescope.lsp_implementations, '[G]oto [I]mplementation')
-          map('gd', uses_direct_navigation and vim.lsp.buf.definition or telescope.lsp_definitions, '[G]oto [D]efinition')
+          map('gd', goto_definition_or_callers, '[G]oto [D]efinition or callers')
           map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
           map('gO', telescope.lsp_document_symbols, 'Open Document Symbols')
           map('gW', telescope.lsp_dynamic_workspace_symbols, 'Open Workspace Symbols')
           map('grt', uses_direct_navigation and vim.lsp.buf.type_definition or telescope.lsp_type_definitions, '[G]oto [T]ype Definition')
-
-          ---@param client vim.lsp.Client
-          ---@param method vim.lsp.protocol.Method
-          ---@param bufnr? integer some lsp support methods only in specific files
-          ---@return boolean
-          local function client_supports_method(client, method, bufnr)
-            if vim.fn.has 'nvim-0.11' == 1 then
-              return client:supports_method(method, bufnr)
-            else
-              return client.supports_method(method, { bufnr = bufnr })
-            end
-          end
 
           if client and uses_csharp_lens and client_supports_method(client, vim.lsp.protocol.Methods.textDocument_codeLens, event.buf) then
             local codelens_augroup = vim.api.nvim_create_augroup('kickstart-lsp-codelens', { clear = false })
