c411f2634a209c9c23576269f814107ae15526ec
sanchith-krishnan <sanchith.krishnan@gmail.com>
2026-05-05 18:41:46 +0530

    CSharp tree sitter setup

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -48,6 +48,14 @@ vim.o.cursorlineopt = 'number'
 
 vim.o.scrolloff = 4
 
+-- Folding: treesitter-driven, start fully unfolded
+vim.o.foldmethod = 'expr'
+vim.o.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+vim.o.foldtext = ''
+vim.o.foldlevel = 99
+vim.o.foldlevelstart = 99
+vim.o.foldenable = true
+
 -- Raise a dialog instead of failing on unsaved changes
 vim.o.confirm = true
 
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,20 +1,12 @@
 return {
   'nvim-treesitter/nvim-treesitter',
+  branch = 'master',
   build = ':TSUpdate',
-  main = 'nvim-treesitter',
+  main = 'nvim-treesitter.configs',
   opts = {
-    ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'python', 'go' },
+    ensure_installed = { 'bash', 'c', 'c_sharp', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'python', 'go' },
     auto_install = true,
+    highlight = { enable = true },
+    indent = { enable = true },
   },
-  config = function(_, opts)
-    require('nvim-treesitter').setup(opts)
-
-    -- Auto-enable treesitter highlighting for supported filetypes (nvim 0.11+)
-    vim.api.nvim_create_autocmd('FileType', {
-      group = vim.api.nvim_create_augroup('treesitter-start', { clear = true }),
-      callback = function(args)
-        pcall(vim.treesitter.start, args.buf)
-      end,
-    })
-  end,
 }
